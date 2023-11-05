e04098641db7f115bc51c254f140b7a7d67d24cc
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 20:54:47 +0100

    Initial commit

---
+++ 003-variable.py
@@ -0,0 +1,11 @@
+# Ask user for his name
+name = input("What's your name? ")
+# Say hello to user
+print("hello, ")
+print(name)
+
+# Say hello to user but in one line (concatenation)
+print("hello, " + name)
+
+# Say hello to user in one line (two arguments)
+print("hello,", name)
